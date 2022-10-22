192577e991d0b6fdf9490a06a1de155ea8e121c7
Anthony Campbell <anthony-campbell@hotmail.com>
2022-10-21 18:28:55 -0700

    Create sudoku board placeholder on frontend.

--- frontend/App.js
+++ frontend/App.js
@@ -7,54 +7,61 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Text
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Sudoku = () => {
+  const blocksArr = Array(9).fill(null);
+  const blocks = blocksArr.map((c, i) => {
+    return <Block key={i} />
+  });
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+    <View className='sudoku' style={styles.sudoku}>{blocks}</View>
+  )
+}
 
-const App: () => Node = () => {
+const Cell = (props) => {
+  return (
+    <Text className='cell' style={styles.cell}>{props.entry}</Text>
+  )
+}
+
+const Block = () => {
+  const cellsArr = Array(9).fill(null);
+  const cells = cellsArr.map((c, i) => {
+    return <Cell key={i} entry={c} />
+  });
+  return (
+    <View className="block" style={styles.block}>{cells}</View>
+  )
+}
+
+const Numbers = () => {
+  const numsArr = Array(9).fill(null);
+  const nums = numsArr.map((n, i) => {
+    return <Num key={i} num={i + 1} />
+  });
+  return (
+    <View style={styles.numbers}>{nums}</View>
+  )
+}
+
+const Num = (props) => {
+  return (
+    <Text style={styles.num}>{props.num}</Text>
+  )
+}
+
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -63,55 +70,55 @@ const App: () => Node = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <View style={{
+        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center"
+      }}>
+        <Sudoku />
+        <Numbers />
+      </View>
+    </SafeAreaView >
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  cell: {
+    width: 25,
+    height: 25,
+    borderWidth: 0.5,
+    borderColor: 'black',
+    textAlign: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  block: {
+    width: 77,
+    height: 77,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    borderWidth: 1,
+    borderColor: 'black'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  sudoku: {
+    width: 233,
+    height: 233,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    borderWidth: 1,
+    borderColor: 'black'
   },
-  highlight: {
-    fontWeight: '700',
+  numbers: {
+    height: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
+  num: {
+    width: 25,
+    height: 25,
+    textAlign: 'center'
+  }
 });
 
 export default App;
