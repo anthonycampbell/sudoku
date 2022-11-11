70de7faf722829ca2cda81650904c6d883f069f4
Anthony Campbell <anthony-campbell@hotmail.com>
2022-11-10 18:26:15 -0800

    Add openCV.

--- frontend/App.js
+++ frontend/App.js
@@ -36,27 +36,16 @@ const App = () => {
     //   [1, null, 7, null, null, 6, null, 8, null],
     //   [8, null, 2, null, 4, 5, null, 9, null]
     // ]
-    // [
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null, null]
-    // ]
     [
-      [null, null, null, null, null, 1, 5, 4, null],
-      [null, null, null, null, null, 7, null, null, null],
-      [3, null, null, null, null, 6, null, null, 8],
-      [null, null, null, null, 6, null, null, null, 8],
-      [7, null, 8, null, null, 9, null, null, null],
-      [null, 2, null, null, 5, null, null, 4, null],
-      [null, null, null, 1, 5, null, 2, null, null],
-      [null, null, null, 3, null, null, null, 1, null],
-      [null, 8, null, null, null, null, null, null, 7]
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null]
     ]
   );
   const isDarkMode = useColorScheme() === 'dark';
