c17c9e0ce1e46c1d6455604efff1525897db5ade
Anthony Campbell <anthony-campbell@hotmail.com>
2022-11-14 16:16:28 -0800

    Edit example sudoku.

--- frontend/App.js
+++ frontend/App.js
@@ -28,26 +28,26 @@ import { launchImageLibrary } from 'react-native-image-picker';
 const App = () => {
   const [board, setBoard] = useState(
     // [
-    //   [5, null, null, null, 4, null, 8, null, null],
-    //   [6, 7, null, 8, null, null, 5, null, null],
-    //   [9, null, null, null, null, null, 6, 1, 3],
-    //   [null, 6, 2, 1, null, null, 3, 7, 4],
-    //   [4, null, null, null, null, 3, 9, null, 8],
-    //   [null, 7, null, null, 2, null, null, null, null],
-    //   [null, 9, 6, 2, 1, 8, null, 5, null],
-    //   [1, null, 7, null, null, 6, null, 8, null],
-    //   [8, null, 2, null, 4, 5, null, 9, null]
+    //   [5, 8, 6, null, null, null, null, null, null],
+    //   [4, null, null, null, 8, null, 9, null, null],
+    //   [null, null, 3, null, null, 4, null, null, 7],
+    //   [null, null, null, null, null, null, null, 3, null],
+    //   [null, null, null, null, null, 9, null, 5, null],
+    //   [null, 4, null, 7, 2, null, null, null, 1],
+    //   [7, null, null, null, 5, null, 2, null, null],
+    //   [null, null, null, null, 3, 2, null, 6, null],
+    //   [null, 6, null, null, null, null, null, null, null]
     // ]
     [
-      [5, 8, 6, null, null, null, null, null, null],
-      [4, null, null, null, 8, null, 9, null, null],
-      [null, null, 3, null, null, 4, null, null, 7],
-      [null, null, null, null, null, null, null, 3, null],
-      [null, null, null, null, null, 9, null, 5, null],
-      [null, 4, null, 7, 2, null, null, null, 1],
-      [7, null, null, null, 5, null, 2, null, null],
-      [null, null, null, null, 3, 2, null, 6, null],
-      [null, 6, null, null, null, null, null, null, null]
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null]
     ]
   );
   const isDarkMode = useColorScheme() === 'dark';
