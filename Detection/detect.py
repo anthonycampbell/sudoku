344d9ac87372e7a6b456f92d2bc35aeda60f2d45
Anthony Campbell <anthony-campbell@hotmail.com>
2022-12-01 23:10:36 -0800

    Correctly recognize 9.

--- Detection/detect.py
+++ Detection/detect.py
@@ -9,9 +9,10 @@ file.write("")
 file.close()
 
 # Load image, grayscale, and adaptive threshold
-image = cv2.imread('sudoku.png')
+image = cv2.imread('sudoku1.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,57,5)
+
 
 # Filter out all numbers and noise to isolate only boxes
 cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -26,6 +27,8 @@ vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
 horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
+
+dilation = cv2.dilate(thresh, vertical_kernel, iterations = 1)
 
 # Sort by top to bottom and each row by left to right
 invert = 255 - thresh
@@ -56,8 +59,11 @@ for row in sudoku_rows:
         x, y, w, h = cv2.boundingRect(c)
         rect = cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cropped = result[y:y + h, x:x + w]
-        text = pytesseract.image_to_string(cropped, config ='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+        cv2.imshow('crop', cropped)
+        cv2.waitKey(1)
+        text = pytesseract.image_to_string(cropped,lang="eng", config='--psm 9 --oem 3 -c tessedit_char_whitelist=0123456789')
         text = text.replace("\n", "")
+        print(text)
         file.write(text or '0')
     file.write('\n')
 file.close
